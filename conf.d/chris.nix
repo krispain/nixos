23edb42007c3581c9498aa6e912501bb202ae9ab
Chris Payne <cpayne@protonmail.ch>
2023-09-20 10:31:58 -0700

    Initial config

---
+++ conf.d/chris.nix
@@ -0,0 +1,69 @@
+{
+  # Set your time zone.
+  time.timeZone = "America/Vancouver";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_CA.UTF-8";
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the Gnome Flashback Desktop Environment.
+  services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.desktopManager.gnome.flashback.enableMetacity = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+
+  environment.systemPackages = with pkgs; [
+     # browsers
+     chromium
+     brave
+
+     # CLI tools
+     mate.mate-terminal
+     neofetch
+     htop
+     ncdu
+     git
+     et
+
+     # GUI tools
+     slack
+     bitwarden
+     signal-desktop
+     authy
+
+     # Office
+     libreoffice
+
+     # Images/sound
+     gimp
+     shotwell
+     audacity
+     sox
+     ffmpeg
+
+     # so we can build our own flatpak, specifically wwphone
+     flatpak-builder
+
+     # Make gnome great again
+     gnome.gnome-tweaks
+
+    # Security
+     protonvpn-cli
+     qbittorrent
+
+  ];
+
+  # flatpak
+  services.flatpak.enable = true;
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+  }; 
+
+}
---
+++ scripts/user-nixos-setup.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+
+# Add flathub
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+######################################
+# wwphone installed via flatpak from #
+# Dan's github repository            #
+# ####################################
+
+# Clone Dan's repo
+git clone https://gitlab.com/shieldwed/ch.wwcom.wwphone.git
+cd ch.wwcom.wwphone/
+
+# need to explicitly clone shared-modules dependancy
+git clone https://github.com/flathub/shared-modules
+
+# flatpak dependencies
+flatpak install flathub org.freedesktop.Sdk
+flatpak install org.freedesktop.Sdk/x86_64/22.08
+flatpak install org.freedesktop.Platform/x86_64/22.08
+
+# build flatpak
+flatpak-builder --repo=/home/cpayne/flatpaks/ build-dir ch.wwcom.wwphone.yaml --force-clean
+
+# add user repository
+flatpak --user remote-add --no-gpg-verify chris-repo /home/cpayne/flatpaks
+
+# install the flatpak from local repo
+flatpak --user install chris-repo ch.wwcom.wwphone
+
+# create config file to set English
+echo <<EOF
+<?xml version="1.0" emcpdomgs="UTF-8"?>
+<CONFIG lang="en" Server="nine.smartpbx.ch"/>
+EOF
+> /home/cpayne/.var/app/ch.wwcom.wwphone/.config/wwphone/wwphone.cfg
+
+# first run
+flatpak run ch.wwcom.wwphone 
+
+# If this file is secured.... add all details
+#
+# echo <<EOF
+# <?xml version="1.0" emcpdomgs="UTF-8"?>
+# <CONFIG lang="en" Server="nine.smartpbx.ch" User="cpayne" Password="PUT THE PASSWORD HERE!!!!!!" mode="1" prefix1="0041" preventTLS="0" active_identity="1" numListViews="1" isExpanded="0" MainFormX="1086" MainFormY="61" lv0w="250"/>
+# > /home/cpayne/.var/app/ch.wwcom.wwphone/.config/wwphone/wwphone.cfg
+
+
+# Some useful links to install stuff
+# Bitwarden brave addon
+# https://chrome.google.com/webstore/detail/bitwarden-free-password-m/nngceckbapebfimnlniiiahkandclblb
+#
+# Manual commands to run/setup
+# authy
+# bitwarden
+# protonvpn-cli login cpayne
+# ssh-keygen -t ed25519
+#
+# Note about undoing 
+# nixos-rebuild switch --upgrade
+#
+# use:
+# nix-channel --rollback
+# nixos-rebuild switch
