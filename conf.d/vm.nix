e8732e625a812b248d6bb3353436f4b90c192502
Chris Payne <cpayne@protonmail.ch>
2023-09-28 22:22:12 -0700

    Add shared filesystem

--- conf.d/vm.nix
+++ conf.d/vm.nix
@@ -2,4 +2,9 @@
   # Spice VDA for clipboard sharing, copy/paste to VM consoles
   services.spice-vdagentd.enable = true;
 
+  # shared with host OS
+  fileSystems."/home/cpayne/shared" =
+    { device = "shared";
+      fsType = "virtiofs";
+    };
 }
