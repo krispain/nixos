ebe7af1d3f085d9d1e9ba693531c8ce6c1e43009
Chris Payne <cpayne@protonmail.ch>
2023-10-01 06:32:03 -0700

    Change mount options to be more robust

--- conf.d/home-nfs.nix
+++ conf.d/home-nfs.nix
@@ -1,17 +1,13 @@
 {
 
-  # ensure mount point exists
-  systemd.tmpfiles.rules = [
-    "d /mnt/WDNFS 0755 root root -"
-  ];
-
   # Big storage at home
   fileSystems."/mnt/WDNFS" =
     { device = "172.16.1.74:/nfs/WDNFS";
       fsType = "nfs";
       options = [
-          "noauto"
-	  "user"
+          "x-systemd.automount"
+	  "noauto"
+	  "x-systemd.idle-timeout=600"
       ];
     };
 }
