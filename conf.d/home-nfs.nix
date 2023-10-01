8d6acc254de553649e022c4c9415c85809de7d24
Chris Payne <cpayne@protonmail.ch>
2023-10-01 06:26:38 -0700

    directory creation

--- conf.d/home-nfs.nix
+++ conf.d/home-nfs.nix
@@ -1,5 +1,10 @@
 {
 
+  # ensure mount point exists
+  systemd.tmpfiles.rules = [
+    "d /mnt/WDNFS 0755 root root -"
+  ];
+
   # Big storage at home
   fileSystems."/mnt/WDNFS" =
     { device = "172.16.1.74:/nfs/WDNFS";
