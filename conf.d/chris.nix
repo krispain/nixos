c367c6b6ca13da199ca3916795e1309235cafca9
Chris Payne <cpayne@protonmail.ch>
2023-10-15 11:36:36 -0700

    test eternal-terminal prioritize - typo

--- conf.d/chris.nix
+++ conf.d/chris.nix
@@ -100,7 +100,7 @@
   ];
 
   # prioritize eternal-terminal et over the egg-timer one
-  pkgs.eternal-terminal.meta.priority = 10;
+  eternal-terminal.meta.priority = 10;
   
   # set default editor to neovim
   programs.neovim = {
