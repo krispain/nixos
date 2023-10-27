202ba6e63d9460911c9ddf124e0b46fa0ffa00fe
Chris Payne <cpayne@protonmail.ch>
2023-10-27 08:54:05 -0700

    more testing gnome-control-center issue

--- conf.d/chris.nix
+++ conf.d/chris.nix
@@ -106,9 +106,9 @@
      gst_all_1.gst-plugins-base
      gst_all_1.gst-plugins-good
      gst_all_1.gstreamer.dev
-     gst_all_1.gst-plugins-bad
-     gst_all_1.gst-plugins-ugly
-     gst_all_1.gst-libav
+#     gst_all_1.gst-plugins-bad
+#     gst_all_1.gst-plugins-ugly
+#     gst_all_1.gst-libav
 
     # Security
      protonvpn-cli
