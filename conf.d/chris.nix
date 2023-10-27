b1f43652b4ea840deeba9fd6dbd8394404fbaec4
Chris Payne <cpayne@protonmail.ch>
2023-10-27 08:42:56 -0700

    more testing gnome-control-center issue

--- conf.d/chris.nix
+++ conf.d/chris.nix
@@ -105,7 +105,7 @@
      gst_all_1.gstreamer
      gst_all_1.gst-plugins-base
      gst_all_1.gst-plugins-good
-#     gst_all_1.gstreamer.dev
+     gst_all_1.gstreamer.dev
      gst_all_1.gst-plugins-bad
      gst_all_1.gst-plugins-ugly
      gst_all_1.gst-libav
