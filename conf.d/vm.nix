f924161987fda000d13aa067771e0e8c472e1071
Chris Payne <cpayne@protonmail.ch>
2023-11-02 10:35:11 -0700

    fixed with virtmanager settings

--- conf.d/vm.nix
+++ conf.d/vm.nix
@@ -2,9 +2,6 @@
   # Spice VDA for clipboard sharing, copy/paste to VM consoles
   services.spice-vdagentd.enable = true;
 
-  # 2023/11/01 gnome flashback broke in VMs, use Mate for now
-  services.xserver.desktopManager.mate.enable = true;
-
   # shared with host OS
   fileSystems."/home/cpayne/shared" =
     { device = "shared";
