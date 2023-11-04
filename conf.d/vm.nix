293b79452c2f1b0d5dbe89a4823a9b3a2d332db3
Chris Payne <cpayne@protonmail.ch>
2023-11-04 13:26:59 -0700

    shared filesystem conf change document

--- conf.d/vm.nix
+++ conf.d/vm.nix
@@ -3,6 +3,8 @@
   services.spice-vdagentd.enable = true;
 
   # shared with host OS
+  # for this to work nixos on nixos, need to add this to the filesystem XML
+  #   <binary path="/run/current-system/sw/bin/virtiofsd"/>
   fileSystems."/home/cpayne/shared" =
     { device = "shared";
       fsType = "virtiofs";
