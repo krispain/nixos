608a59f7f46f3861a2f4ebbeea04a495cbb89c57
Chris Payne <cpayne@protonmail.ch>
2023-11-06 19:52:49 -0800

    Rework with nixos env vars

--- conf.d/nine-wireguard.nix
+++ conf.d/nine-wireguard.nix
@@ -6,42 +6,45 @@
     rules."route-wg-vpn" = {
       onState = ["routable" "off"];
       script = ''
-        #!${pkgs.runtimeShell}
+#!${pkgs.runtimeShell}
 
-        case $2 in
-          up)
-            if ip link show dev $1 &>> /dev/null; then
-              ip route flush table $1 || true
-	      case $1 in
-	        wg_nine)
-                  ip route|grep default | sed -e 's/.*via \(.*\) dev.*/\1/' | xargs -L1 ip route add 178.209.34.137 via
-		  ;;
-		wg_nine34)
-                  ip route|grep default | sed -e 's/.*via \(.*\) dev.*/\1/' | xargs -L1 ip route add 5.148.185.104 via
-		  ;;
-                *)
-                  :
-                ;;
-	      esac
-            fi
-          ;;
-          down)
-            case $1 in
-              wg_nine)
-	        ip route|grep default | sed -e 's/.*via \(.*\) dev.*/\1/' | xargs -L1 ip route del 178.209.34.137 via
-	        ;;
-	      wg_nine34)
-	        ip route|grep default | sed -e 's/.*via \(.*\) dev.*/\1/' | xargs -L1 ip route del 5.148.185.104 via
-              ;;
-              *)
-                :
-              ;;
-            esac
-          ;;
-          *)
-            :
-          ;;
-        esac
+case $OperationalState in
+  routable)
+    case $IFACE in
+      wg_nine)
+        if ip link show dev $IFACE &>> /dev/null; then
+          ip route flush table $IFACE || true
+          ip route|grep default | sed -e 's/.*via \(.*\) dev.*/\1/' | xargs -L1 ip route add 178.209.34.137 via
+	fi
+      ;;
+      wg_nine34)
+        if ip link show dev $IFACE &>> /dev/null; then
+          ip route flush table $IFACE || true
+          ip route|grep default | sed -e 's/.*via \(.*\) dev.*/\1/' | xargs -L1 ip route add 5.148.185.104 via
+	fi
+      ;;
+      *)
+        :
+      ;;
+    esac
+  ;;
+  off)
+    case $IFACE in
+      wg_nine)
+        ip route|grep default | sed -e 's/.*via \(.*\) dev.*/\1/' | xargs -L1 ip route del 178.209.34.137 via
+        ;;
+      wg_nine34)
+        ip route|grep default | sed -e 's/.*via \(.*\) dev.*/\1/' | xargs -L1 ip route del 5.148.185.104 via
+      ;;
+      *)
+        :
+      ;;
+    esac
+  ;;
+  *)
+    :
+  ;;
+esac
       '';
     };
   };
