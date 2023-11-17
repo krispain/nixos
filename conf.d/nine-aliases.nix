03f1abf2cc0a63f7a501afbf8b37fdf69d7b616e
Chris Payne <cpayne@protonmail.ch>
2023-11-17 10:14:34 -0800

    Console command ;

--- conf.d/nine-aliases.nix
+++ conf.d/nine-aliases.nix
@@ -4,7 +4,7 @@
     m="ssh cpayne.nine.ch";
     mo="mosh --no-init cpayne.nine.ch";
     gs="git status";
-    console="javaws -J-Djava.security.properties=/home/cpayne/.config/icedtea-web/disabled_tls.properties"
+    console="javaws -J-Djava.security.properties=/home/cpayne/.config/icedtea-web/disabled_tls.properties";
   };
 
 }
