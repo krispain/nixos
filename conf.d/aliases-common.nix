88634dd8ba0dc27326f5036e8e1590e4359beb77
Chris Payne <cpayne@protonmail.ch>
2023-11-17 10:30:57 -0800

    separate common aliases

---
+++ conf.d/aliases-common.nix
@@ -0,0 +1,9 @@
+{
+
+  environment.shellAliases = {
+    gs="git status";
+    myip="dig +short myip.opendns.com @resolver1.opendns.com";
+    view="vim -R";
+  };
+
+}
--- conf.d/nine-aliases.nix
+++ conf.d/nine-aliases.nix
@@ -3,9 +3,7 @@
   environment.shellAliases = {
     m="ssh cpayne.nine.ch";
     mo="mosh --no-init cpayne.nine.ch";
-    gs="git status";
     console="javaws -J-Djava.security.properties=/home/cpayne/.config/icedtea-web/disabled_tls.properties";
-    myip="dig +short myip.opendns.com @resolver1.opendns.com";
   };
 
 }
