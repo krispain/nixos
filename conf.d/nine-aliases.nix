d43a3b9cc56e4a309df50ca6127c5f1f41db55ef
Chris Payne <cpayne@protonmail.ch>
2023-11-17 10:16:29 -0800

    my ip command

--- conf.d/nine-aliases.nix
+++ conf.d/nine-aliases.nix
@@ -5,6 +5,7 @@
     mo="mosh --no-init cpayne.nine.ch";
     gs="git status";
     console="javaws -J-Djava.security.properties=/home/cpayne/.config/icedtea-web/disabled_tls.properties";
+    myip="dig +short myip.opendns.com @resolver1.opendns.com";
   };
 
 }
