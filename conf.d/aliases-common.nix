c892d57f18f7f4a2b42892caa22a4a2539e0a6cc
Chris Payne <cpayne@protonmail.ch>
2023-11-19 17:16:21 -0800

    alias to see what changed

--- conf.d/aliases-common.nix
+++ conf.d/aliases-common.nix
@@ -4,6 +4,7 @@
     gs="git status";
     myip="dig +short myip.opendns.com @resolver1.opendns.com";
     view="vim -R";
+    changes="nvd diff $(ls -trd /nix/var/nix/profiles/*-link|tail -n 2)";
   };
 
 }
