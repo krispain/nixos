5d1ab35caa203b324941d24f0dfb9aee063b098c
Chris Payne <cpayne@protonmail.ch>
2024-01-11 08:47:37 -0800

    Add vnc client

--- conf.d/docker.nix
+++ conf.d/docker.nix
@@ -3,6 +3,7 @@
   virtualisation.docker.enable = true;
   environment.systemPackages = with pkgs; [
      docker-compose
+     tigervnc	# for VNC to containers if needed
   ];
 
   users.extraGroups.docker.members = [ "cpayne" ];
