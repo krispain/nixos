ffde349f0261a019ef58a2aaa67a6ff6b5d5e6e8
Chris Payne <cpayne@protonmail.ch>
2024-02-06 19:45:50 -0800

    TPM for Windows VMs

--- conf.d/kvm.nix
+++ conf.d/kvm.nix
@@ -11,7 +11,7 @@
   security.tpm2.enable = true;
   security.tpm2.pkcs11.enable = true;  # expose /run/current-system/sw/lib/libtpm2_pkcs11.so
   security.tpm2.tctiEnvironment.enable = true;  # TPM2TOOLS_TCTI and TPM2_PKCS11_TCTI env variables
-  users.users.YOUR_USER.extraGroups = [ "tss" ];  # tss group has access to TPM devices
+  users.users.cpayne.extraGroups = [ "tss" ];  # tss group has access to TPM devices
 # need to run these 2 to get networking running and on by default
 # virsh net-autostart default
 # virsh net-start default
