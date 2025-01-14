5dca2ffb5c3ee67621842654649654742c10c2b0
Chris Payne <cpayne@protonmail.ch>
2025-01-14 09:32:33 -0800

    Update change alias to be SMRT

--- conf.d/aliases-common.nix
+++ conf.d/aliases-common.nix
@@ -4,7 +4,8 @@
     gs="git status";
     myip="dig +short myip.opendns.com @resolver1.opendns.com";
     view="vim -R";
-    changes="nvd diff $(ls -trd /nix/var/nix/profiles/*-link|tail -n 2)";
+    changes="if test \$(stat -c %Y -- $(ls -trd /nix/var/nix/profiles/*-link|tail -n 1)) -gt \$(($EPOCHSECONDS - 120))  ; then nvd diff \$(ls -trd /nix/var/nix/profiles/*-link|tail -n 2); else echo 'No recent changes' ; fi"
+
   };
 
   environment.sessionVariables = {
