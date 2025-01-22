a5680ec3ac9239dad4f9a793841cc32bfe2d89c9
Chris Payne <cpayne@protonmail.ch>
2025-01-22 12:32:48 -0800

    Add update alias

--- conf.d/aliases-common.nix
+++ conf.d/aliases-common.nix
@@ -6,6 +6,7 @@
     view="vim -R";
     changes="if test \$(stat -c %Y -- \$(ls -trd /nix/var/nix/profiles/*-link|tail -n 1)) -gt \$(($EPOCHSECONDS - 120))  ; then nvd diff \$(ls -trd /nix/var/nix/profiles/*-link|tail -n 2); else echo 'No recent changes' ; fi";
     changes-force="nvd diff \$(ls -trd /nix/var/nix/profiles/*-link|tail -n 2)";
+    nixos-update="cd /etc/nixos/chris/ && git pull && nixos-rebuild switch --upgrade && echo && changes ; echo ; date"
   };
 
   environment.sessionVariables = {
