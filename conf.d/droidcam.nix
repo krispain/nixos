1b845deb920dac4f457d85c3285214cb6c3bec27
Chris Payne <cpayne@protonmail.ch>
2025-03-04 12:49:31 -0800

    Add obs for droidcam support

---
+++ conf.d/droidcam.nix
@@ -0,0 +1,15 @@
+# /etc/nixos/configuration.nix
+{ pkgs }:
+
+{
+  # Install OBS Studio with droidcam-obs
+  programs.obs-studio = {
+    enable = true;
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+    ];
+    enableVirtualCamera = true;
+  };
+}
