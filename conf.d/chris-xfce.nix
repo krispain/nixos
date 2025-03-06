624ede351f3ae6eb7aec90d14023511e06c05516
Chris Payne <cpayne@protonmail.ch>
2025-03-06 15:22:33 -0800

    Add XFCE config fie

---
+++ conf.d/chris-xfce.nix
@@ -0,0 +1,207 @@
+{ config, lib, pkgs, ... }:
+
+  # use unstable protonvpn-gui
+  #
+  # Also need to manually add the channel as root:
+  # nix-channel --add https://nixos.org/channels/nixos-unstable nixos-unstable
+  # nix-channel --update
+  let
+    unstable = import <nixos-unstable> {};
+  in 
+  {
+
+  # Set your time zone.
+  time.timeZone = "America/Vancouver";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  services.xserver = {
+    enable = true;
+    desktopManager = {
+      xterm.enable = false;
+      xfce.enable = true;
+    };
+  };
+  services.displayManager.defaultSession = "xfce";
+
+  # Configure keymap in X11
+  services.xserver = {
+    xkb.layout = "us";
+    xkb.variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+  };
+
+  nixpkgs.config.permittedInsecurePackages = [
+    "qbittorrent-4.6.4"
+  ];
+
+  # enables support for SANE scanners
+  hardware.sane.enable = true; 
+  hardware.sane.extraBackends = [ pkgs.sane-airscan ];
+
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+  };
+
+  environment.systemPackages = with pkgs; [
+     # browsers
+     chromium
+     firefox-esr-unwrapped
+
+     # CLI tools
+     mate.mate-terminal
+     neofetch
+     htop
+     gtop
+     ncdu
+     git
+     et
+     parted
+     zip
+     unzip
+     file
+     wget
+     traceroute
+     android-tools
+     (lib.hiPrio pkgs.eternal-terminal) # higher priority than egg timer 
+     mosh
+     fwupd
+     nvd
+     bintools
+     rclone
+     whois
+     dig
+     cron
+     bc
+     nmap
+     qrrs
+
+     # GUI tools
+     nextcloud-client
+     meld
+     adoptopenjdk-icedtea-web
+     openvpn
+     glxinfo
+     linphone
+     mate.atril
+     gparted
+     lsof
+     gettext
+
+     # Office
+     libreoffice
+
+     # Images/sound
+     gimp-with-plugins
+     shotwell
+     mate.mate-utils
+     mate.eom
+     audacity
+     sox
+     ffmpeg
+     ffmpeg-normalize
+     vlc
+     unstable.yt-dlp
+     id3v2
+     cdrkit
+     lame
+     id3lib
+     mp3gain
+     simple-scan
+
+     # so we can build our own flatpak, specifically wwphone
+     flatpak-builder
+
+     # Make gnome great again
+     gnome-tweaks
+     gnome-applets
+     networkmanagerapplet
+     gst_all_1.gstreamer
+     gst_all_1.gst-plugins-base
+     gst_all_1.gst-plugins-good
+     gst_all_1.gstreamer.dev
+     gst_all_1.gst-plugins-bad
+     gst_all_1.gst-plugins-ugly
+     gst_all_1.gst-libav
+     # gst_all_1.gst-vaapi
+
+     # cinnamon
+     libgtop
+
+    # Security
+     unstable.protonvpn-gui
+     wireguard-tools
+     qbittorrent
+
+    # zram for "swap"
+    pkgs.zram-generator
+
+  ];
+
+
+  # set default editor to neovim
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+    configure = {
+      customRC = ''
+        syntax on
+	set mouse=r
+      '';
+    };
+  };
+
+  # flatpak
+  services.flatpak.enable = true;
+
+  # fwupd
+  services.fwupd.enable = true;
+ 
+  # cron
+  services.cron.enable = true;
+
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+  }; 
+
+  # enable zram swap
+  zramSwap.enable = true;
+
+  # Add users bin to path
+  environment.localBinInPath = true;
+
+  # sudo doesn't need a password to burn cds
+  security.sudo.extraRules= [
+    {  users = [ "cpayne" ];
+      commands = [
+         { command = "/run/current-system/sw/bin/wodim" ;
+           options= [ "NOPASSWD" ]; 
+        }
+      ];
+    }
+  ];
+}
