37b4a56dac04d54f59f6831b6123b659e8bdfb31
Chris Payne <cpayne@protonmail.ch>
2025-04-04 16:39:59 -0700

    fix relative path

--- conf.d/nine-wireguard.nix
+++ conf.d/nine-wireguard.nix
@@ -3,7 +3,7 @@
 # setup the routing for the nine wireguard VPNs
       networking.networkmanager.dispatcherScripts = [ 
 	{ 
-          source = ./scripts/dispatcher.sh;
+          source = ../scripts/dispatcher.sh;
 	  type = "basic";
      	} 
      ];
