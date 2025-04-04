33ed65167b945f3e92e3efb4b3408dca2a822df5
Chris Payne <cpayne@protonmail.ch>
2025-04-04 15:50:52 -0700

    Update network dispatcher script

--- conf.d/nine-wireguard.nix
+++ conf.d/nine-wireguard.nix
@@ -8,19 +8,38 @@
       script = ''
 #!${pkgs.runtimeShell}
 
-case $AdministrativeState in
-  configured)
-    if ip link show dev wg_nine &>> /dev/null; then
-      ip route flush table wg_nine || true
-      ip route|grep default | sed -e 's/.*via \(.*\) dev.*/\1/' | xargs -L1 ip route add 178.209.34.137 via
-    elif ip link show dev nine_es34 &>> /dev/null; then
-      ip route flush table nine_es34 || true
-      ip route|grep default | sed -e 's/.*via \(.*\) dev.*/\1/' | xargs -L1 ip route add 5.148.185.104 via
-    fi
+set -o errexit
+set -o pipefail
+
+declare -A IF_IDS
+IF_IDS=(
+  [wg_nine]=25910
+  [wg_nine34]=25911
+)
+
+case $2 in
+  up)
+    for ifdev in "${!IF_IDS[@]}"; do
+      if ip link show dev "${ifdev}" &>> /dev/null; then
+        ip route flush table "${IF_IDS[$ifdev]}" || true
+        ip -4 route show | grep default | sed -E "s/$/ table ${IF_IDS[$ifdev]}/" | xargs -L1 ip route add
+        ip rule show priority "${IF_IDS[$ifdev]}" | grep -q fwmark || ip rule add from all fwmark "${IF_IDS[$ifdev]}" table "${IF_IDS[$ifdev]}" priority "${IF_IDS[$ifdev]}"
+      fi
+    done
+  ;;
+  down)
+    case $1 in
+      wg_nine | wg_nine34)
+        ip rule delete priority "${IF_IDS[$1]}" || true
+        ip route flush table "${IF_IDS[$1]}"
+      ;;
+      *)
+        :
+      ;;
+    esac
   ;;
   *)
-   ip route|grep default | sed -e 's/.*via \(.*\) dev.*/\1/' | xargs -L1 ip route del 178.209.34.137 via
-   ip route|grep default | sed -e 's/.*via \(.*\) dev.*/\1/' | xargs -L1 ip route del 5.148.185.104 via
+    :
   ;;
 esac
       '';
