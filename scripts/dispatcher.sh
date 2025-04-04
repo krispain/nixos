3393426f2edb3cc7e0fafabd0180607e87452f34
Chris Payne <cpayne@protonmail.ch>
2025-04-04 16:48:33 -0700

    explicit path to 'ip'

--- scripts/dispatcher.sh
+++ scripts/dispatcher.sh
@@ -3,6 +3,7 @@
 set -o errexit
 set -o pipefail
 
+IP="/run/current-system/sw/bin/ip"
 declare -A IF_IDS
 IF_IDS=(
   [wg_nine]=25910
@@ -12,18 +13,18 @@ IF_IDS=(
 case $2 in
   up)
     for ifdev in "${!IF_IDS[@]}"; do
-      if ip link show dev "${ifdev}" &>> /dev/null; then
-        ip route flush table "${IF_IDS[$ifdev]}" || true
-        ip -4 route show | grep default | sed -E "s/$/ table ${IF_IDS[$ifdev]}/" | xargs -L1 ip route add
-        ip rule show priority "${IF_IDS[$ifdev]}" | grep -q fwmark || ip rule add from all fwmark "${IF_IDS[$ifdev]}" table "${IF_IDS[$ifdev]}" priority "${IF_IDS[$ifdev]}"
+      if ${IP} link show dev "${ifdev}" &>> /dev/null; then
+        ${IP} route flush table "${IF_IDS[$ifdev]}" || true
+        ${IP} -4 route show | grep default | sed -E "s/$/ table ${IF_IDS[$ifdev]}/" | xargs -L1 ${IP} route add
+        ${IP} rule show priority "${IF_IDS[$ifdev]}" | grep -q fwmark || ${IP} rule add from all fwmark "${IF_IDS[$ifdev]}" table "${IF_IDS[$ifdev]}" priority "${IF_IDS[$ifdev]}"
       fi
     done
   ;;
   down)
     case $1 in
       wg_nine | wg_nine34)
-        ip rule delete priority "${IF_IDS[$1]}" || true
-        ip route flush table "${IF_IDS[$1]}"
+        ${IP} rule delete priority "${IF_IDS[$1]}" || true
+        ${IP} route flush table "${IF_IDS[$1]}"
       ;;
       *)
         :
