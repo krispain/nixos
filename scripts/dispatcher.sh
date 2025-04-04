a60f6d79ca35d37c898962edba1c47e58ac74ef2
Chris Payne <cpayne@protonmail.ch>
2025-04-04 16:37:42 -0700

    split scriout out

---
+++ scripts/dispatcher.sh
@@ -0,0 +1,36 @@
+#!${pkgs.runtimeShell}
+
+set -o errexit
+set -o pipefail
+
+declare -A IF_IDS
+IF_IDS=(
+  [wg_nine]=25910
+  [wg_nine34]=25911
+)
+
+case $2 in
+  up)
+    for ifdev in "${!IF_IDS[@]}"; do
+      if ip link show dev "${ifdev}" &>> /dev/null; then
+        ip route flush table "${IF_IDS[$ifdev]}" || true
+        ip -4 route show | grep default | sed -E "s/$/ table ${IF_IDS[$ifdev]}/" | xargs -L1 ip route add
+        ip rule show priority "${IF_IDS[$ifdev]}" | grep -q fwmark || ip rule add from all fwmark "${IF_IDS[$ifdev]}" table "${IF_IDS[$ifdev]}" priority "${IF_IDS[$ifdev]}"
+      fi
+    done
+  ;;
+  down)
+    case $1 in
+      wg_nine | wg_nine34)
+        ip rule delete priority "${IF_IDS[$1]}" || true
+        ip route flush table "${IF_IDS[$1]}"
+      ;;
+      *)
+        :
+      ;;
+    esac
+  ;;
+  *)
+    :
+  ;;
+esac
