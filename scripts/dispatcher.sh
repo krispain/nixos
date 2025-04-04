b94635716855c84d76c73d4c2ab89f65ec9a968d
Chris Payne <cpayne@protonmail.ch>
2025-04-04 16:52:44 -0700

    explicit path to tools

--- scripts/dispatcher.sh
+++ scripts/dispatcher.sh
@@ -4,6 +4,8 @@ set -o errexit
 set -o pipefail
 
 IP="/run/current-system/sw/bin/ip"
+GREP="/run/current-system/sw/bin/grep"
+SED="/run/current-system/sw/bin/sed"
 declare -A IF_IDS
 IF_IDS=(
   [wg_nine]=25910
@@ -15,8 +17,8 @@ case $2 in
     for ifdev in "${!IF_IDS[@]}"; do
       if ${IP} link show dev "${ifdev}" &>> /dev/null; then
         ${IP} route flush table "${IF_IDS[$ifdev]}" || true
-        ${IP} -4 route show | grep default | sed -E "s/$/ table ${IF_IDS[$ifdev]}/" | xargs -L1 ${IP} route add
-        ${IP} rule show priority "${IF_IDS[$ifdev]}" | grep -q fwmark || ${IP} rule add from all fwmark "${IF_IDS[$ifdev]}" table "${IF_IDS[$ifdev]}" priority "${IF_IDS[$ifdev]}"
+        ${IP} -4 route show | ${GREP} default | ${SED} -E "s/$/ table ${IF_IDS[$ifdev]}/" | xargs -L1 ${IP} route add
+        ${IP} rule show priority "${IF_IDS[$ifdev]}" | ${GREP} -q fwmark || ${IP} rule add from all fwmark "${IF_IDS[$ifdev]}" table "${IF_IDS[$ifdev]}" priority "${IF_IDS[$ifdev]}"
       fi
     done
   ;;
