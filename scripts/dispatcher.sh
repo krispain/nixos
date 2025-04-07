8cedea21212bd6b19b2821fce3ea59ad0f955a2c
Chris Payne <cpayne@protonmail.ch>
2025-04-07 11:36:49 -0700

    Add needed route

--- scripts/dispatcher.sh
+++ scripts/dispatcher.sh
@@ -6,10 +6,16 @@ set -o pipefail
 IP="/run/current-system/sw/bin/ip"
 GREP="/run/current-system/sw/bin/grep"
 SED="/run/current-system/sw/bin/sed"
+AWK="/run/current-system/sw/bin/awk"
 declare -A IF_IDS
 IF_IDS=(
   [wg_nine]=25910
   [wg_nine34]=25911
+)
+declare -A IF_IPS
+IF_IPS=(
+  [wg_nine]=178.209.34.137
+  [wg_nine34]=5.148.185.104
 )
 
 case $2 in
@@ -18,6 +24,7 @@ case $2 in
       if ${IP} link show dev "${ifdev}" &>> /dev/null; then
         ${IP} route flush table "${IF_IDS[$ifdev]}" || true
         ${IP} -4 route show | ${GREP} default | ${SED} -E "s/$/ table ${IF_IDS[$ifdev]}/" | xargs -L1 ${IP} route add
+        ${IP} -4 route show | ${GREP} default | ${AWK} '{print $3}' | xargs -L1 route add "${IF_IPS[$ifdev]}" via 
         ${IP} rule show priority "${IF_IDS[$ifdev]}" | ${GREP} -q fwmark || ${IP} rule add from all fwmark "${IF_IDS[$ifdev]}" table "${IF_IDS[$ifdev]}" priority "${IF_IDS[$ifdev]}"
       fi
     done
