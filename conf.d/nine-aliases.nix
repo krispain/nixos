738f6f6f467c52f5fcf8b525bd168ba2b1953f13
Chris Payne <cpayne@protonmail.ch>
2025-04-12 21:33:18 -0700

    Update docs and aliases

--- conf.d/nine-aliases.nix
+++ conf.d/nine-aliases.nix
@@ -3,7 +3,16 @@
   environment.shellAliases = {
     m="ssh cpayne.nine.ch";
     mo="mosh --no-init cpayne.nine.ch";
+    cz41="ssh cpayne-cz41.nine.ch";
+    cz41m="mosh --no-init cpayne-cz41.nine.ch";
+    cz41e="et cpayne-cz41.nine.ch";
+    cz42="ssh cpayne-cz42.nine.ch";
+    cz42m="mosh --no-init cpayne-cz42.nine.ch";
+    cz42e="et cpayne-cz42.nine.ch";
+    es34="ssh cpayne-es34.nine.ch";
+    es34m="mosh --no-init cpayne-es34.nine.ch";
+    es34e="et cpayne-es34.nine.ch";
     console="javaws -J-Djava.security.properties=/home/cpayne/.config/icedtea-web/disabled_tls.properties";
   };
 
-}
+
