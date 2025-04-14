09740cf4b31f53253fb1a6d2427dc6a7b2234956
Chris Payne <cpayne@protonmail.ch>
2025-04-14 09:39:42 -0700

    Add clean alias

--- conf.d/aliases-common.nix
+++ conf.d/aliases-common.nix
@@ -7,6 +7,7 @@
     changes="if test \$(stat -c %Y -- \$(ls -trd /nix/var/nix/profiles/*-link|tail -n 1)) -gt \$(($EPOCHSECONDS - 120))  ; then nvd diff \$(ls -trd /nix/var/nix/profiles/*-link|tail -n 2); else echo 'No recent changes' ; fi";
     changes-force="nvd diff \$(ls -trd /nix/var/nix/profiles/*-link|tail -n 2)";
     nixos-update="cd /etc/nixos/chris/ && git pull && nixos-rebuild switch --upgrade && echo && changes ; echo ; date";
+    nixos-clean="time nix-collect-garbage -d && time nix-store --optimize";
   };
 
   environment.sessionVariables = {
