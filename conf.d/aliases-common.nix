98ce8e1d5aeada287c3c9d0b4f47aa78a53aa879
Chris Payne <cpayne@protonmail.ch>
2025-11-14 12:06:37 -0800

    Add channel update to update alias

--- conf.d/aliases-common.nix
+++ conf.d/aliases-common.nix
@@ -6,7 +6,7 @@
     view="vim -R";
     changes="if test \$(stat -c %Y -- \$(ls -trd /nix/var/nix/profiles/*-link|tail -n 1)) -gt \$(($EPOCHSECONDS - 120))  ; then nvd diff \$(ls -trd /nix/var/nix/profiles/*-link|tail -n 2); else echo 'No recent changes' ; fi";
     changes-force="nvd diff \$(ls -trd /nix/var/nix/profiles/*-link|tail -n 2)";
-    nixos-update="cd /etc/nixos/chris/ && git pull && nixos-rebuild build --upgrade && echo && nvd diff /run/current-system ./result ; echo ; echo \"Run:\" ; echo; echo \"nixos-rebuild switch\"; echo; echo \"to activate\"; echo ; echo \"also update flatpaks using:\" ; echo ; echo \"flatpak update -y\"; echo ;  date";
+    nixos-update="cd /etc/nixos/chris/ && git pull && nix-channel --update && nixos-rebuild build --upgrade && echo && nvd diff /run/current-system ./result ; echo ; echo \"Run:\" ; echo; echo \"nixos-rebuild switch\"; echo; echo \"to activate\"; echo ; echo \"also update flatpaks using:\" ; echo ; echo \"flatpak update -y\"; echo ;  date";
     nixos-clean="time nix-collect-garbage --delete-older-than 7d && time nix-store --optimize";
   };
 
