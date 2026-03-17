25c0d58f4d8fb38e1e256388f7b7cf0edc276e8f
Chris Payne <cpayne@protonmail.ch>
2026-03-17 16:00:18 -0700

    Add openvpn2.4

---
+++ conf.d/openvpn2.4.nix
@@ -0,0 +1,14 @@
+{
+  stdenv,
+  fetchzip,
+}:
+
+stdenv.mkDerivation {
+  pname = "openvpn24";
+  version = "2.4.12";
+
+  src = fetchzip {
+    url = "https://build.openvpn.net/downloads/releases/openvpn-2.4.12.tar.gz";
+    sha256 = "";
+  };
+}
