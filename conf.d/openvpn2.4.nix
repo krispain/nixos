4f13a9e3f805c5f1f9060376776f73e9a1eeccc4
Chris Payne <cpayne@protonmail.ch>
2026-03-18 09:26:31 -0700

    try with AI - deepseek

--- conf.d/openvpn2.4.nix
+++ conf.d/openvpn2.4.nix
@@ -1,27 +1,31 @@
 { config, pkgs, ... }:
 
 let
-  openvpn = pkgs.stdenv.mkDerivation {
-    pname = "openvpn";
-    version = "2.4.12";
-
-    src = pkgs.fetchurl {
-      url = "https://build.openvpn.net/downloads/releases/openvpn-2.4.12.tar.gz";
-      sha256 = "e7734c97501c44a2a8df69a243dde26e";
-    };
-
-    configureFlags = [
-      "--prefix=${pkgs.stdenv.lib.makeStorePath "${pkgs.stdenv.hostName}"}"
-    ];
-
-    buildInputs = [];
-
-    meta = {
-      description = "OpenVPN";
-      homepage = "https://openvpn.net/";
-      license = pkgs.lib.licenses.gpl3;
-    };
+  # Fetch and unpack the OpenVPN source tarball
+  openvpnSource = builtins.fetchTarball {
+    url = "https://build.openvpn.net/downloads/releases/openvpn-2.4.12.tar.gz";
+    sha256 = "e7734c97501c44a2a8df69a243dde26e";
   };
 
+  # Build the OpenVPN package
+  openvpn = pkgs.stdenv.mkDerivation {
+    name = "openvpn-2.4.12";
+    src = openvpnSource;
+
+    buildInputs = with pkgs; [ openssl curl ];
+
+    configurePhase = ''
+      ./configure --prefix=$out
+    '';
+
+    buildPhase = "make";
+
+    installPhase = ''
+      make install
+    '';
+  };
 in
-  openvpn
+{
+  # Define the package in your system configuration
+  environment.systemPackages = [ openvpn ];
+}
