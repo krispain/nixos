d9c8cdd0a38c649ffe43b7de36ea230778f9dc27
Chris Payne <cpayne@protonmail.ch>
2026-03-18 08:29:52 -0700

    file formatting

--- conf.d/openvpn2.4.nix
+++ conf.d/openvpn2.4.nix
@@ -1,14 +1,16 @@
 {
-  stdenv,
-  fetchzip,
-}:
+  {
+    stdenv,
+    fetchzip,
+  }:
 
-stdenv.mkDerivation {
-  pname = "openvpn24";
-  version = "2.4.12";
-
-  src = fetchzip {
-    url = "https://build.openvpn.net/downloads/releases/openvpn-2.4.12.tar.gz";
-    sha256 = "";
-  };
+  stdenv.mkDerivation {
+    pname = "openvpn24";
+    version = "2.4.12";
+   
+    src = fetchzip {
+      url = "https://build.openvpn.net/downloads/releases/openvpn-2.4.12.tar.gz";
+      sha256 = "";
+    };
+  }
 }
