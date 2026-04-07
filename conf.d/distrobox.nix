3f3c5946f3b7ec6c5f02bc522f566a5ed47a8944
Chris Payne <cpayne@protonmail.ch>
2026-04-07 11:02:13 -0700

    Add podman for distrobox

--- conf.d/distrobox.nix
+++ conf.d/distrobox.nix
@@ -1,7 +1,23 @@
 { pkgs, ... }:
 {
+
+  virtualisation = {
+    containers.enable = true;
+    podman = {
+      enable = true;
+      dockerCompat = true;
+      defaultNetwork.settings.dns_enabled = true; # Required for containers under podman-compose to be able to talk to each other.
+    };
+  };
+  
+  users.users.cpayne = {
+    extraGroups = [
+      "podman"
+    ];
+  }
+
   environment.systemPackages = with pkgs; [
      distrobox
-  ];
 
+  ];
 }
