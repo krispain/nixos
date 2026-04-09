855ca608f2f5d44c3a739a604fc40476e87ec862
Chris Payne <cpayne@protonmail.ch>
2026-04-09 10:54:34 -0700

    Add IPv6 myip command, fix ipv4 version

--- conf.d/aliases-common.nix
+++ conf.d/aliases-common.nix
@@ -2,7 +2,8 @@
 
   environment.shellAliases = {
     gs="git status";
-    myip="dig +short myip.opendns.com @resolver1.opendns.com";
+    myip="dig -4 +short myip.opendns.com @resolver1.opendns.com";
+    myip6="dig -t AAAA +short myip.opendns.com @resolver1.opendns.com";
     view="vim -R";
     changes="if test \$(stat -c %Y -- \$(ls -trd /nix/var/nix/profiles/*-link|tail -n 1)) -gt \$(($EPOCHSECONDS - 120))  ; then nvd diff \$(ls -trd /nix/var/nix/profiles/*-link|tail -n 2); else echo 'No recent changes' ; fi";
     changes-force="nvd diff \$(ls -trd /nix/var/nix/profiles/*-link|tail -n 2)";
