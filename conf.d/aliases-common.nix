32c8374b1c0667ad6935b1efbbcd3724ead211b0
Chris Payne <cpayne@protonmail.ch>
2026-04-09 11:23:06 -0700

    Add full clean alias

--- conf.d/aliases-common.nix
+++ conf.d/aliases-common.nix
@@ -9,6 +9,7 @@
     changes-force="nvd diff \$(ls -trd /nix/var/nix/profiles/*-link|tail -n 2)";
     nixos-update="cd /etc/nixos/chris/ && git pull && nix-channel --update && nixos-rebuild build --upgrade && echo && nvd diff /run/current-system ./result ; echo ; echo \"Run:\" ; echo; echo \"nixos-rebuild switch\"; echo; echo \"to activate\"; echo ; echo \"also update flatpaks using:\" ; echo ; echo \"flatpak update -y\"; echo ;  date";
     nixos-clean="time nix-collect-garbage --delete-older-than 7d && time nix-store --optimize";
+    nixos-full-clean="time rm -v /nix/var/nix/gcroots/auto/* && nix-collect-garbage -d && time nix-store --optimize";
   };
 
   environment.sessionVariables = {
