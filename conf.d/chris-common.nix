469f23f3b81cc9809663e8e73e7490b5c6d159cd
Chris Payne <cpayne@protonmail.ch>
2026-05-01 12:38:45 -0700

    remove from common, redundant and causing issues on aarch64

--- conf.d/chris-common.nix
+++ conf.d/chris-common.nix
@@ -146,14 +146,6 @@
      gnome-tweaks
      gnome-applets
      networkmanagerapplet
-     gst_all_1.gstreamer
-     gst_all_1.gst-plugins-base
-     gst_all_1.gst-plugins-good
-     gst_all_1.gstreamer.dev
-     gst_all_1.gst-plugins-bad
-     gst_all_1.gst-plugins-ugly
-     gst_all_1.gst-libav
-     # gst_all_1.gst-vaapi
 
      # cinnamon
      libgtop
