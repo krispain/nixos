c59603e8cfeeacde6297aafe7445b17a5000daa3
Chris Payne <cpayne@protonmail.ch>
2026-06-09 20:12:20 -0700

    Add old buffalo NAS

--- conf.d/home-nfs.nix
+++ conf.d/home-nfs.nix
@@ -10,4 +10,13 @@
 	  "x-systemd.idle-timeout=600"
       ];
     };
+  fileSystems."/mnt/storage-3" =
+    { device = "172.16.1.250:/storage-3";
+      fsType = "nfs";
+      options = [
+          "x-systemd.automount"
+	  "noauto"
+	  "x-systemd.idle-timeout=600"
+      ];
+    };
 }
